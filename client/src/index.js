80542f0ee8129b770f72d4bd1dadffd6517fdc26
smiska <Mihly Sry>
2019-10-25 16:39:02 +0200

    build pwa ready except redirect, own worker not used

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+// import * as serviceWorker from './serviceWorker';
+
 
 import App from './components/App';
 import reducers from './reducers';
@@ -19,3 +21,5 @@ ReactDOM.render(
   </Provider>,
   document.querySelector('#root')
 );
+
+// serviceWorker.register();
